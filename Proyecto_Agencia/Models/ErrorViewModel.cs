5bc937f0bf8aff9b4e87ce284aa0d67b42d38fdf
perezhernandezjazziel183@gmail.com <perezhernandezjazziel183@gmail.com>
2023-02-06 16:10:18 -0600

    Add project files.

---
+++ Proyecto_Agencia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Agencia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
